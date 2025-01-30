9d3de13493dcc7174257dab90629a399a2fe992e
n-github2005 <kadudulanamitha@gmail.com>
2025-01-30 22:20:12 +0530

    Add files via upload

---
+++ Strlength.java
@@ -0,0 +1,8 @@
+public class Strlength {
+    public static void main(String[] args) {
+        String str="Hello World";
+        int length=str.length();
+        System.out.println("length:"+length);
+    }
+    
+}
